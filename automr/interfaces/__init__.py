e5f1b507337e48e9b874b4b0c2e0aee4ebe245d1
G.C.M. <charithmanujaya1@gmail.com>
2026-06-06 01:49:17 +0530

    init for interfaces

---
+++ automr/interfaces/__init__.py
@@ -0,0 +1,11 @@
+from .model_interface import BaseModel
+from .transform_interface import BaseTransform
+from .relation_interface import BaseRelation
+from .comparator_interface import BaseComparator
+
+__all__ = [
+    "BaseModel",
+    "BaseTransform",
+    "BaseRelation",
+    "BaseComparator"
+]
