b5d1b74ac154ddbdb43833aeb39e7a8d6fb55c58
G.C.M. <charithmanujaya1@gmail.com>
2026-06-08 03:32:42 +0530

    new

--- automr/models/custom_wrapper.py
+++ automr/models/custom_wrapper.py
@@ -1,6 +1,4 @@
 from automr.interfaces import BaseModel
-
-
 class CustomWrapper(BaseModel):
 
     def __init__(self, model):
@@ -8,3 +6,10 @@ class CustomWrapper(BaseModel):
 
     def predict(self, x):
         return self.model.predict(x)
+
+    def predict_batch(self, xs):
+
+        if hasattr(self.model, "predict_batch"):
+            return self.model.predict_batch(xs)
+
+        return [self.model.predict(x) for x in xs]
