009add9295d63d57e8332a47e423aadc5716f001
G.C.M. <charithmanujaya1@gmail.com>
2026-06-08 03:41:09 +0530

    new

--- automr/core/model_wrapper.py
+++ automr/core/model_wrapper.py
@@ -5,8 +5,23 @@ class ModelWrapper:
         self.predict_fn = predict_fn
 
     def predict(self, x):
-        ...
-    
+
+        if x is None:
+            return 0.0
+
+        if self.predict_fn:
+            y = self.predict_fn(self.model, x)
+        else:
+            y = self.model.predict(x)
+
+        if isinstance(y, (list, tuple)):
+            return float(y[0])
+
+        if hasattr(y, "shape"):
+            return float(y.flatten()[0])
+
+        return float(y)
+
     def predict_batch(self, xs):
 
         if hasattr(self.model, "predict_batch"):
