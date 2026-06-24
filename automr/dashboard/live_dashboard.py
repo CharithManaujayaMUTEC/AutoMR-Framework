320abcd181acd33613ff00b72c4bdc51a0e8d51d
G.C.M. <charithmanujaya1@gmail.com>
2026-06-24 13:04:08 +0530

    live dashbord

---
+++ automr/dashboard/live_dashboard.py
@@ -0,0 +1,14 @@
+class LiveDashboard:
+
+    def __init__(
+        self,
+        automr,
+        model,
+        selected_mrs,
+        custom_ranges,
+        frame_skip,
+        save_results,
+        save_violations,
+        output_dir
+    ):
+        ...
