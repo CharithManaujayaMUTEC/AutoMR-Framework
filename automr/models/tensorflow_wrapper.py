5a959e356dd388b04c0e2ab4d7441512c70d576b
G.C.M. <charithmanujaya1@gmail.com>
2026-06-25 12:27:44 +0530

    tf wrapper updated and fixed

--- automr/models/tensorflow_wrapper.py
+++ automr/models/tensorflow_wrapper.py
@@ -1,3 +1,4 @@
+import numpy as np
 from automr.interfaces import BaseModel
 
 
@@ -7,5 +8,11 @@ class TensorFlowWrapper(BaseModel):
         self.model = model
 
     def predict(self, x):
+        x = np.asarray(x, dtype=np.float32)
+
+        # If a single image (64,64,3) is passed, convert it to (1,64,64,3)
+        if x.ndim == 3:
+            x = np.expand_dims(x, axis=0)
+
         pred = self.model.predict(x, verbose=0)
         return pred
